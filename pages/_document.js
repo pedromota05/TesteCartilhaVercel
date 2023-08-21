94bc56710ab7a6943413ddecb74839f51208012e
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-08-21 15:40:59 -0400

    instalação funcionando de novo

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <Link rel="manifest" href="/manifest.json" />
-        <Link rel="apple-touch-icon" href="/icon-512x512.png"></Link>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png"/>
         <meta name="theme-color" content="#000" />
         {/* Importação de Links */}
         <Link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
