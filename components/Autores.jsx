1832f2f23bba72f22d6e7ae831c8248a52005b08
Pedro Mota <94080428+pedromota05@users.noreply.github.com>
2023-08-22 13:46:31 -0300

    Update Autores.jsx

--- components/Autores.jsx
+++ components/Autores.jsx
@@ -60,7 +60,7 @@ export const Autores = () => {
                                 <input
                                     className="form-control border-0 rounded-pill pr-5"
                                     type="search"
-                                    placeholder="Search"
+                                    placeholder="Buscar"
                                     aria-label="Search"
                                 />
                                 <div className="input-group-append">
@@ -105,7 +105,7 @@ export const Autores = () => {
                                     <input
                                         className="form-control border-0 rounded-pill pr-5"
                                         type="search"
-                                        placeholder="Search"
+                                        placeholder="Buscar"
                                         aria-label="Search"
                                     />
                                     <div className="input-group-append">
