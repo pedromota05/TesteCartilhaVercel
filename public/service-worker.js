912f7e238a1df549c8474f42075e1e794c0fc29f
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2023-09-18 12:25:27 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,47 +1,46 @@
 // service-worker.js
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
+// Rota para a API de capítulos
 workbox.routing.registerRoute(
-    // Define a rota da sua API, incluindo os parâmetros de consulta
-    new RegExp('https://api-cartilha.onrender.com/api/capitulos\\?populate=.*'),
-    // Use a estratégia de cache-first
-    new workbox.strategies.NetworkFirst({
-      cacheName: 'api-cache',
-      // plugins: [
-      //   // Configura o cache para armazenar apenas respostas com status 200 (OK)
-      //   new workbox.cacheableResponse.CacheableResponsePlugin({
-      //     statuses: [200],
-      //   }),
-      // ],
-    })
-  );
+  new RegExp('https://api-cartilha.onrender.com/api/capitulos\\?populate=.*'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'api-capitulos-cache',
+  })
+);
 
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/api/capitulos')) {
-      const promiseChain = fetch(event.request.clone())
-        .catch(() => {
-          return self.registration.sync.register('syncData');
-        });
-      event.waitUntil(promiseChain);
-    }
-  });
+// Rota para a API de autores
+workbox.routing.registerRoute(
+  new RegExp('https://api-cartilha.onrender.com/api/autors.*'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'api-autores-cache',
+  })
+);
 
-  self.addEventListener('sync', (event) => {
-    if (event.tag === 'syncData') {
-      event.waitUntil(syncData());
-    }
-  });
-  
-  function syncData() {
-    return workbox.precaching.cleanupOutdatedCaches()
-      .then(() => {
-        return workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/capitulos') || event.request.url.includes('/api/autors')) {
+    const promiseChain = fetch(event.request.clone())
+      .catch(() => {
+        return self.registration.sync.register('syncData');
       });
+    event.waitUntil(promiseChain);
   }
-  
-  
+});
 
-// Define outras rotas para arquivos estáticos, como ícones e assets
+self.addEventListener('sync', (event) => {
+  if (event.tag === 'syncData') {
+    event.waitUntil(syncData());
+  }
+});
+
+function syncData() {
+  return workbox.precaching.cleanupOutdatedCaches()
+    .then(() => {
+      return workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+    });
+}
+
+// Rotas para arquivos estáticos
 workbox.routing.registerRoute(
   /\.(?:png|jpg|jpeg|svg|gif|ico|css)$/,
   new workbox.strategies.CacheFirst({
@@ -49,7 +48,7 @@ workbox.routing.registerRoute(
   })
 );
 
-// Define a estratégia para o arquivo de manifest
+// Rota para o arquivo de manifest
 workbox.routing.registerRoute(
   /manifest.json$/,
   new workbox.strategies.StaleWhileRevalidate({
@@ -57,7 +56,7 @@ workbox.routing.registerRoute(
   })
 );
 
-// Define a estratégia para outras rotas (página principal, etc.)
+// Rota para outras rotas (página principal, etc.)
 workbox.routing.registerRoute(
   ({ url }) => url.origin === self.location.origin,
   new workbox.strategies.StaleWhileRevalidate()
