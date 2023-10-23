03ebce8de8be3266721b8ba2bddde8b22e6a4c83
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2023-10-23 14:13:19 -0300

    componente funcionando

--- components/TextCapitulos.jsx
+++ components/TextCapitulos.jsx
@@ -1,13 +1,24 @@
 import { useState, useEffect, useRef } from 'react';
 import TableOfContents from './TableOfContents';
 
-const TextCapitulos = ({lista, activeTitle, setActiveTitle}) => {
+const TextCapitulos = ({ lista, activeTitle, setActiveTitle }) => {
   const [headerBlocks, setHeaderBlocks] = useState([]);
+  const [references, setReferences] = useState([]);
 
   useEffect(() => {
     const extractedHeaderBlocks = [];
+    const extractedReferences = [];
+
     lista.forEach((cap) => {
       const blocks = JSON.parse(cap.attributes.description).blocks;
+      const refs = cap.attributes.referencias; // Assuming referencias is an array
+
+      if (refs && refs.length > 0) {
+        const refBlocks = JSON.parse(refs[0].description).blocks;
+        extractedReferences.push(refBlocks);
+
+        console.log(refBlocks);
+      }
       blocks.forEach((block) => {
         if (block.type === 'header') {
           extractedHeaderBlocks.push(block);
@@ -15,107 +26,25 @@ const TextCapitulos = ({lista, activeTitle, setActiveTitle}) => {
       });
     });
     setHeaderBlocks(extractedHeaderBlocks);
+    setReferences(extractedReferences);
     console.log("headerBlocks:", extractedHeaderBlocks);
   }, [lista]);
+  
 
   function convertToHTML(data) {
     let htmlContent = ''; // Variável para armazenar o conteúdo HTML
-    let isInstituicao = false;
-    let isInsideEmbrapaMilhoESorgo = false;
-    
+
     data.blocks.forEach((block) => {
       switch (block.type) {
         case 'header':
           const anchor = block.data.text.replace(/ /g, "_"); // Criar âncora
           htmlContent += `<h${block.data.level} class="titulo" id='${anchor}'>${block.data.text}</h${block.data.level}>`;
           break;
-          case 'paragraph':
-            if (block.data.text.includes("Embrapa Gado de Leite")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            }
-            else if (block.data.text.includes("Embrapa Agropecuária Oeste")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            } 
-            else if (block.data.text.includes("Agência de Desenvolvimento Agrário e Extensão Rural (Agraer)")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            } 
-            else if (block.data.text.includes("Universidade Federal da Grande Dourados (UFGD)")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            }
-            else if (block.data.text.includes("Embrapa Milho e Sorgo")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            } 
-            else if (block.data.text.includes("Embrapa Hortaliças")) {
-              // Se encontrarmos o início do bloco de instituição, defina isInstituicao como verdadeiro
-              isInstituicao = true;
-              htmlContent += `<div class="instituicao">`;
-              htmlContent += `<p class="nome-instituicao">${block.data.text}</p>`;
-            } 
-            else if (isInstituicao) {
-              const parts = block.data.text.split('Site institucional:');
-              const parts1 = block.data.text.split('Fone:');
-              const parts2 = block.data.text.split('Local:');
-              if (parts.length === 2) {
-                // Se houver dois pontos, aplique a formatação desejada
-                const boldText = parts[0].trim();
-                const normalText = parts[1].trim();
-                htmlContent += `<p><strong>${boldText}Site institucional:</strong> ${normalText}</p>`;
-              } 
-              else if (parts1.length === 2) {
-                // Se houver dois pontos, aplique a formatação desejada
-                const boldText = parts1[0].trim();
-                const normalText = parts1[1].trim();
-                htmlContent += `<p><strong>${boldText}Fone:</strong> ${normalText}</p>`;
-              } 
-              else if (parts2.length === 2) {
-                // Se houver dois pontos, aplique a formatação desejada
-                const boldText = parts2[0].trim();
-                const normalText = parts2[1].trim();
-                htmlContent += `<p><strong>${boldText}Local:</strong> ${normalText}</p>`;
-              } 
-              else {
-                // Se não houver dois pontos, adicione o conteúdo normal com a classe "paragrafo"
-                htmlContent += `<p class="paragrafo">${block.data.text}</p>`;
-              }
-            } else {
-              // Se não estivermos no bloco de instituição, adicione o conteúdo normal com a classe "paragrafo"
-              htmlContent += `<p class="paragrafo">${block.data.text}</p>`;
-            }
-            
-            // Verifique se o bloco de instituição foi fechado
-            if (isInstituicao && block.data.text.includes("Local:Dourados, MS")) {
-              htmlContent += `</div>`; // Feche a div do bloco de instituição
-              isInstituicao = false; // Redefina isInstituicao
-            } else if (isInstituicao && block.data.text.includes("Local: Juiz de Fora, MG")) {
-              htmlContent += `</div>`; // Feche a div do bloco de instituição
-              isInstituicao = false; // Redefina isInstituicao
-            } else if (isInstituicao && block.data.text.includes("Local:Campo Grande, MS")) {
-              htmlContent += `</div>`; // Feche a div do bloco de instituição
-              isInstituicao = false; // Redefina isInstituicao
-            } else if (isInstituicao && block.data.text.includes("Local:Sete Lagoas, MG")) {
-              htmlContent += `</div>`; // Feche a div do bloco de instituição
-              isInstituicao = false; // Redefina isInstituicao
-            }
-            else if (isInstituicao && block.data.text.includes("Local:Brasília, DF")) {
-              htmlContent += `</div>`; // Feche a div do bloco de instituição
-              isInstituicao = false; // Redefina isInstituicao
-            }
-            break;
+        case 'paragraph':
+
+          // Se não estivermos no bloco de instituição, adicione o conteúdo normal com a classe "paragrafo"
+          htmlContent += `<p class="paragrafo">${block.data.text}</p>`;
+          break;
         case 'list':
           const listType = block.data.style === 'ordered' ? 'ol' : 'ul';
           let listItemsHTML = '';
@@ -124,22 +53,22 @@ const TextCapitulos = ({lista, activeTitle, setActiveTitle}) => {
           });
           htmlContent += `<${listType} class="lista">${listItemsHTML}</${listType}>`;
           break;
-         case 'image':
-            // Use a URL do Cloudinary fornecida no bloco de dados
-            const imageSrc = block.data.file.url;
-            const imageCaption = block.data.caption;
-  
-            // Crie o elemento de imagem com a URL do Cloudinary
-            htmlContent += `<img src="${imageSrc}" alt="${imageCaption}" />`;
-            htmlContent += `<p class="legenda-img">${imageCaption}</p>`;
-            break;
-          case 'embed':
-            const videoUrl = new URL(block.data.source);
-            const videoId = videoUrl.pathname.substring(1); // Remove a barra inicial
-            const videoCaption = block.data.caption;
-            const videoEmbedUrl = `https://www.youtube.com/embed/${videoId}`;
-              htmlContent +=
-                ` <div id="player">
+        case 'image':
+          // Use a URL do Cloudinary fornecida no bloco de dados
+          const imageSrc = block.data.file.url;
+          const imageCaption = block.data.caption;
+
+          // Crie o elemento de imagem com a URL do Cloudinary
+          htmlContent += `<img src="${imageSrc}" alt="${imageCaption}" />`;
+          htmlContent += `<p class="legenda-img">${imageCaption}</p>`;
+          break;
+        case 'embed':
+          const videoUrl = new URL(block.data.source);
+          const videoId = videoUrl.pathname.substring(1); // Remove a barra inicial
+          const videoCaption = block.data.caption;
+          const videoEmbedUrl = `https://www.youtube.com/embed/${videoId}`;
+          htmlContent +=
+            ` <div id="player">
                     <div class="html5-video-player">
                       <iframe
                         width="100%"
@@ -151,12 +80,36 @@ const TextCapitulos = ({lista, activeTitle, setActiveTitle}) => {
                       </iframe>
                     </div>
                   </div>`
-            break;
-          // Adicione outros casos para outros tipos de blocos do Editor.js, se necessário.
-          default:
-            break;
+          break;
+        // Adicione outros casos para outros tipos de blocos do Editor.js, se necessário.
+        default:
+          // const anchor1 = block.data.text.replace(/ /g, "_"); // Criar âncora
+          // htmlContent += `<h${block.data.level} class="titulo" id='${anchor}'>${block.data.text}</h${block.data.level}>`;
+          break;
       }
     });
+    return htmlContent;
+  }
+
+  function RefconvertToHTML(data) {
+    let htmlContent = ''; // Variável para armazenar o conteúdo HTML
+    htmlContent += `<div class='instituicao'>`
+    data.blocks.forEach((block) => {
+      switch (block.type) {
+        case 'header':
+          const anchor = block.data.text.replace(/ /g, "_"); // Criar âncora
+          htmlContent += `<h${block.data.level} class="nome-instituicao" id='${anchor}'>${block.data.text}</h${block.data.level}>`;
+          break;
+        case 'paragraph':
+          htmlContent += `<p class="paragrafo">${block.data.text}</p>`;
+          break;
+        default:
+          const anchor1 = block.data.text.replace(/ /g, "_"); // Criar âncora
+          htmlContent += `<h${block.data.level} class="titulo" id='${anchor}'>${block.data.text}</h${block.data.level}>`;
+          break;
+      }
+    });
+    htmlContent += `</div>`
     return htmlContent;
   }
   const chapterRefs = useRef({}); // Use useRef para armazenar referências a elementos de capítulo
@@ -190,6 +143,15 @@ const TextCapitulos = ({lista, activeTitle, setActiveTitle}) => {
                 )}
                 {activeTitle === cap.id && (
                   <div dangerouslySetInnerHTML={{ __html: convertToHTML(JSON.parse(cap.attributes.description)) }} />
+                )}
+                <h3>Instituição</h3>
+                 {/* Adicione esta seção para exibir as referências */}
+                 {activeTitle === cap.id && references.length > 0 && (
+                  <div className="references">
+                    <h2>{references[0].title}</h2>
+                    <div dangerouslySetInnerHTML={{ __html: RefconvertToHTML({ blocks: references[0] }) }} />
+                    {console.log("info das ref", references)}
+                  </div>
                 )}
               </div>
             ))}
