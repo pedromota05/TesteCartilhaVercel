5adde083255403fd3c32793e1c4952ce3c13bf9f
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-04-05 16:14:30 -0400

    mudando config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,61 @@
-const withPWA = require('next-pwa');
-const path = require('path');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development'
+})
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  },
+/** @type {import('next').NextConfig} */
+const path = require('path')
+ 
+const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-});
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+module.exports = nextConfig
+
+// Injected content via Sentry wizard below
+
+// const { withSentryConfig } = require("@sentry/nextjs");
+
+// module.exports = withSentryConfig(
+//   module.exports,
+//   {
+//     // For all available options, see:
+//     // https://github.com/getsentry/sentry-webpack-plugin#options
+
+//     // Suppresses source map uploading logs during build
+//     silent: true,
+//     org: "tecnofam",
+//     project: "pwa",
+//     url: "https://bug.embrapa.io/"
+//   },
+//   {
+//     // For all available options, see:
+//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+//     // Upload a larger set of source maps for prettier stack traces (increases build time)
+//     widenClientFileUpload: true,
+
+//     // Transpiles SDK to be compatible with IE11 (increases bundle size)
+//     transpileClientSDK: true,
+
+//     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+//     tunnelRoute: "/monitoring",
+
+//     // Hides source maps from generated client bundles
+//     hideSourceMaps: true,
+
+//     // Automatically tree-shake Sentry logger statements to reduce bundle size
+//     disableLogger: true,
+
+//     // Enables automatic instrumentation of Vercel Cron Monitors.
+//     // See the following for more information:
+//     // https://docs.sentry.io/product/crons/
+//     // https://vercel.com/docs/cron-jobs
+//     automaticVercelMonitors: true,
+//   }
+// );
