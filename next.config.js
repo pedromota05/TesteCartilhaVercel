d7209a9838afe7d13c3c9f8a5bd2abe4a563d90f
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-04-05 16:27:30 -0400

    corrigindo offline

--- next.config.js
+++ next.config.js
@@ -1,61 +1,19 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development'
-})
-
-/** @type {import('next').NextConfig} */
-const path = require('path')
- 
+// Configuration options for Next.js
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true,      // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
-}
+};
 
-module.exports = nextConfig
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
 
-// Injected content via Sentry wizard below
-
-// const { withSentryConfig } = require("@sentry/nextjs");
-
-// module.exports = withSentryConfig(
-//   module.exports,
-//   {
-//     // For all available options, see:
-//     // https://github.com/getsentry/sentry-webpack-plugin#options
-
-//     // Suppresses source map uploading logs during build
-//     silent: true,
-//     org: "tecnofam",
-//     project: "pwa",
-//     url: "https://bug.embrapa.io/"
-//   },
-//   {
-//     // For all available options, see:
-//     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-//     // Upload a larger set of source maps for prettier stack traces (increases build time)
-//     widenClientFileUpload: true,
-
-//     // Transpiles SDK to be compatible with IE11 (increases bundle size)
-//     transpileClientSDK: true,
-
-//     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-//     tunnelRoute: "/monitoring",
-
-//     // Hides source maps from generated client bundles
-//     hideSourceMaps: true,
-
-//     // Automatically tree-shake Sentry logger statements to reduce bundle size
-//     disableLogger: true,
-
-//     // Enables automatic instrumentation of Vercel Cron Monitors.
-//     // See the following for more information:
-//     // https://docs.sentry.io/product/crons/
-//     // https://vercel.com/docs/cron-jobs
-//     automaticVercelMonitors: true,
-//   }
-// );
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
